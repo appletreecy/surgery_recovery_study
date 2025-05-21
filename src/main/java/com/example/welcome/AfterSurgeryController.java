0fd3bd7d1c7b7b4740d4ae6fd40f521bc5a71269
appletreecy <appletreecy@gmail.com>
2025-05-21 16:16:12 +1000

    20250521

---
+++ src/main/java/com/example/welcome/AfterSurgeryController.java
@@ -0,0 +1,46 @@
+package com.example.welcome;
+
+import com.example.welcome.model.AfterSurgery;
+import com.example.welcome.repository.AfterSurgeryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("afterSurgery")
+public class AfterSurgeryController {
+
+    @Autowired
+    private AfterSurgeryRepository afterSurgeryRepository;
+
+    @GetMapping("/add")
+    public String showAddForm(Model model){
+        model.addAttribute("afterSurgery", new AfterSurgery());
+        return "add-afterSurgery";
+    }
+
+    @PostMapping("/add")
+    public String submitForm(@ModelAttribute AfterSurgery afterSurgery){
+        afterSurgeryRepository.save(afterSurgery);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete")
+    public String showDeleteForm() {
+        return "delete-afterSurgery";
+    }
+
+    @PostMapping("/delete")
+    public String deleteRecord(@RequestParam("id") Long id, Model model) {
+        boolean exists = afterSurgeryRepository.existsById(id);
+        if (exists) {
+            afterSurgeryRepository.deleteById(id);
+            model.addAttribute("message", "Record with ID " + id + " has been deleted.");
+        } else {
+            model.addAttribute("message", "No record found with ID " + id + ".");
+        }
+        return "delete-afterSurgery";
+    }
+
+}
